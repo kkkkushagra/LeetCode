263503318484553506d162de756da1f68fb4a579
Kushagra Trivedi <143111324+kkkkushagra@users.noreply.github.com>
2026-07-16 09:24:39 +0530

    Time: 0 ms (100%), Space: 46.8 MB (33.31%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,14 +9,21 @@
  *     }
  * }
  */
-public class Solution {
+class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> s = new HashSet<>();
-        while(head!=null){
-            if(s.contains(head)==true) return true;
-            s.add(head);
-            head=head.next;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
         }
+
         return false;
     }
 }
